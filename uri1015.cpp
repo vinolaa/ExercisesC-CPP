fb2f71da3f6f4a4da31b67ec4bfa30fcf46d25e0
Vinicius Oliveira <46850868+vinolaa@users.noreply.github.com>
2021-03-25 21:01:30 -0300

    Add files via upload

---
+++ uri1015.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    double X1, X2, Y1, Y2, D;
+    scanf("%lf %lf", &X1, &Y1);
+    scanf("%lf %lf", &X2, &Y2);
+    D = sqrt(pow(X1-X2,2) + (pow(Y2-Y1,2)));
+    printf("%.4lf\n", D);
+    return 0;
+}
