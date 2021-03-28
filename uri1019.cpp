63fbd0750941cebc762def6ed91b1bf786f67936
Vinicius Oliveira <46850868+vinolaa@users.noreply.github.com>
2021-03-28 00:13:55 -0300

    Add files via upload

---
+++ uri1019.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+	int N, h, m, s;
+	scanf("%d", &N);
+	
+	h = (N/3600); 
+	
+	m = (N -(3600*h))/60;
+	
+	s = (N -(3600*h)-(m*60));
+	
+	printf("%d:%d:%d\n", h, m, s);
+	
+	return 0;
+}
