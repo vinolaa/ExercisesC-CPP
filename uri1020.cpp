8a0e3e4606c8bd69b2e50e597fdce82f5573f6c7
Vinicius Oliveira <46850868+vinolaa@users.noreply.github.com>
2021-03-29 21:36:12 -0300

    Add files via upload

---
+++ uri1020.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int idade, ano, mes, dia;
+    scanf("%d", &idade);
+    ano = idade/365;
+    mes = (idade - (365*ano))/30;
+    dia = (idade - (365*ano) - (mes*30));
+    printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", ano, mes, dia);
+    return 0;
+}
